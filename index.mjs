687c9426477c689e90d2d9e69d4f6fc46d21d161
timqian <timqian@t9t.io>
2023-03-22 23:33:58 +0800

    init script

---
+++ index.mjs
@@ -0,0 +1,48 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { createClient } from "@supabase/supabase-js";
+import { writeFileSync } from "fs";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+const { data, error } = await supabase.from("prompts").select(`
+	name,
+	description,
+	messages,
+	handles!handles_prompt_id_fkey(
+		handle
+	),
+	created_by:profiles!prompts_created_by_fkey(
+		handles!handles_user_id_fkey(
+			handle
+		)
+	),
+	star_count:user-star-prompts(
+		count
+	),
+	created_at
+`);
+
+const result = data
+  .sort((a, b) => b.star_count[0].count - a.star_count[0].count)
+	.filter((prompt) => prompt.messages && prompt.messages[0])
+  .slice(0, 30)
+  .map((prompt) => {
+    return {
+      name: prompt.name,
+      description: prompt.description,
+      prompt:
+        prompt.messages && prompt.messages[0]
+          ? prompt.messages[0].content
+          : null,
+      handle: prompt.handles.handle,
+      created_by: prompt.created_by.handles.handle,
+      star_count: prompt.star_count[0].count,
+    };
+  });
+
+writeFileSync("TopPrompts.json", JSON.stringify(result, null, 2));
